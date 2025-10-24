c0863f1c8ca002c2e6ce6bb053a34036ec2fa447
yishai <yishai5555@gmail.com>
2025-10-24 15:27:06 +0300

    04-first-test-fix-stack-name

--- py-testing/tests/unit/test_py_testing_stack.py
+++ py-testing/tests/unit/test_py_testing_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from py_testing.py_testing_stack import PyTestingStack
+from py_testing.py_testing_stack import PySimpleStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in py_testing/py_testing_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = PyTestingStack(app, "py-testing")
+    stack = PySimpleStack(app, "py-testing")
     template = assertions.Template.from_stack(stack)
 
 
