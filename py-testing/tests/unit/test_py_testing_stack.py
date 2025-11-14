e6ecd1d69aeabc20ce23df9d7aec3292cb4a38d1
yishai <yishai5555@gmail.com>
2025-11-14 15:02:00 +0200

    10-captor

--- py-testing/tests/unit/test_py_testing_stack.py
+++ py-testing/tests/unit/test_py_testing_stack.py
@@ -3,7 +3,8 @@ import aws_cdk.assertions as assertions
 import pytest
 
 
-from aws_cdk.assertions import Match
+from aws_cdk.assertions import Match, Capture
+
 from py_testing.py_testing_stack import PySimpleStack
 
 
@@ -23,10 +24,12 @@ def test_lambda_props(simple_template):
 
     simple_template.resource_count_is("AWS::Lambda::Function", 1)
 
+
 def test_lambda_runtime_with_matcher(simple_template):
     simple_template.has_resource_properties("AWS::Lambda::Function", {
         "Runtime": Match.string_like_regexp("python*"),
     })
+
 
 def test_lambda_runtime_with_matcher2(simple_template):
     simple_template.has_resource_properties("AWS::Lambda::Function", {
@@ -34,7 +37,6 @@ def test_lambda_runtime_with_matcher2(simple_template):
     })
 
     # simple_template.resource_count_is("AWS::Lambda::Function", 1)
-
 
 
 def test_lambda_bucket_with_matchers(simple_template):
@@ -48,7 +50,8 @@ def test_lambda_bucket_with_matchers(simple_template):
                             "Resource": [
                                 {
                                     "Fn::GetAtt": [
-                                        Match.string_like_regexp("SimpleBucket"),
+                                        Match.string_like_regexp(
+                                            "SimpleBucket"),
                                         "Arn",
                                     ]
                                 },
@@ -60,3 +63,33 @@ def test_lambda_bucket_with_matchers(simple_template):
             }
         ),
     )
+
+
+def test_lambda_actions_with_captors(simple_template):
+    lambda_action_captor = Capture()
+
+    simple_template.has_resource_properties(
+        "AWS::IAM::Policy",
+        {
+            "PolicyDocument": {
+                "Statement": [
+                    {
+                        "Action": lambda_action_captor,
+                    }
+                ]
+            }
+        }
+    )
+
+    expected_actions = [
+        "s3:GetBucket*",
+        "s3:GetObject*",
+        "s3:List*"
+    ]
+
+    assert sorted(lambda_action_captor.as_array()) == sorted(expected_actions)
+
+    assert set(lambda_action_captor.as_array()) == set(expected_actions)
+
+
+    assert "s3:GetObject" in expected_actions
