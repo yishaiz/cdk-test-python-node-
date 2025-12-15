a76f25a149ae15597ecaac6a8dd3ecb4dee05813
yishai <yishai5555@gmail.com>
2025-12-15 18:17:58 +0200

    17-tags

--- py-testing/app.py
+++ py-testing/app.py
@@ -7,8 +7,16 @@ from py_testing.py_testing_stack import PySimpleStack
 
 
 app = cdk.App()
-PySimpleStack(app, "PySimpleStack",
+my_stack = PySimpleStack(app, "PySimpleStack")
 
-               )
+cdk.Tags.of(my_stack).add("stage", "testing")
+
+cdk.Tags.of(my_stack).add("storage", "main",
+                          include_resource_types=["AWS::S3::Bucket"],
+                          priority=200)
+
+cdk.Tags.of(my_stack).add("storage", "aux",
+                          include_resource_types=["AWS::S3::Bucket"])
+
 
 app.synth()
