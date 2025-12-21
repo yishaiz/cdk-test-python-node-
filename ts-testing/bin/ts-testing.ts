e130812e6f6ae7a2264e1ba45bc65723af2809b9
yishai <yishai5555@gmail.com>
2025-12-21 18:30:29 +0200

    a

--- ts-testing/bin/ts-testing.ts
+++ ts-testing/bin/ts-testing.ts
@@ -3,6 +3,15 @@ import * as cdk from 'aws-cdk-lib';
 import { TsSimpleStack } from '../lib/ts-testing-stack';
 
 const app = new cdk.App();
-new TsSimpleStack(app, 'TsTestingStack', {
-  
+const tsSimpleStack = new TsSimpleStack(app, 'TsTestingStack');
+
+cdk.Tags.of(tsSimpleStack).add('stage', 'test');
+
+cdk.Tags.of(tsSimpleStack).add('stage', 'main', {
+  includeResourceTypes: ['AWS::S3::Bucket'],
+});
+
+cdk.Tags.of(tsSimpleStack).add('stage', 'aux', {
+  includeResourceTypes: ['AWS::S3::Bucket'],
+  priority: 150,
 });
