0a77551c4d1480adbdc8439060b753e4474ef546
yishai <yishai5555@gmail.com>
2025-12-21 19:31:46 +0200

    19

--- py-testing/py_testing/policy_check.py
+++ py-testing/py_testing/policy_check.py
@@ -1,9 +1,22 @@
 import jsii
-from aws_cdk import IAspect
+import json
+from aws_cdk import IAspect, aws_iam, Stack, Annotations
 
 
 @jsii.implements(IAspect)
 class PolicyChecker:
     def visit(self, node):
-        print(f"Visiting node: {node}")
-        print(f"Visiting: {node.__class__.__name__}")
+        if isinstance(node, aws_iam.CfnPolicy):
+            resolvedDoc = Stack.of(node).resolve(node.policy_document)
+            resolvedDocJson = json.dumps(resolvedDoc)
+
+            print(resolvedDocJson)
+
+            if 'GetBucket' in resolvedDocJson:
+                Annotations.of(node).add_warning_v2(
+                    "PolicyChecker:GetBucketNotAllowed",  # id
+                    "IAM Policy contains 'GetBucket' action, which is not allowed."
+                )
+
+        # print(f"Visiting: {node.__class__.__name__}")
+        # print(f"Visiting node: {node}")
